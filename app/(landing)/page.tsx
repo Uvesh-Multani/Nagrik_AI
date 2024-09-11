d8f1b3ab9734323cc17d0540f3e3a0e50299622e
Uvesh Multani <cypronium2004@gmail.com>
2024-09-11 20:57:58 +0530

    Update 4 Sep 3

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { LandingNagrik } from "@/components/landing-nagrik";
 import { Subscribe } from "@/components/subscribe";
+import Script from "next/script";  // Import Script from next/script
 
 const LandingPage = () => {
   return (
@@ -13,9 +14,27 @@ const LandingPage = () => {
       <LandingHero />
       <LandingNagrik />
       <LandingContent />
-      <Subscribe /> 
+      <Subscribe />
+
+      <Script
+        id="chatbot-config"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.embeddedChatbotConfig = {
+              chatbotId: "w8Cwg_NuSV5mr2UUvEC_i",
+              domain: "www.chatbase.co"
+            };
+          `,
+        }}
+      />
+
+      <Script
+        src="https://www.chatbase.co/embed.min.js"
+        strategy="afterInteractive"
+        defer
+      />
     </div>
-    
   );
 };
 
